356526cb5b7a9fefcbdaaaf9dee5e9f9c348a789
lufizz <693680512@qq.com>
2019-12-28 00:45:45 +0800

    项目构建

---
+++ ltParent/ltsr/src/main/java/com/ltlufizz/ltsr/LtsrApplication.java
@@ -0,0 +1,13 @@
+package com.ltlufizz.ltsr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LtsrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LtsrApplication.class, args);
+    }
+
+}
---
+++ ltParent/ltsr/src/test/java/com/ltlufizz/ltsr/LtsrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ltlufizz.ltsr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LtsrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
